77ca176ee9ecb4f208445558c4c31100ec9c888a
bhavyasharmahere <thebhavya85@gmail.com>
2026-01-18 21:05:36 +0530

    V-F

--- calc.py
+++ calc.py
@@ -9,65 +9,57 @@ student_class = int(input("Enter Class: "))
 
 if student_class >= 8:
     section = input("Enter Section: ")
-    print("\nEnter Maximum Marks for each subject:")
-    max_comp = int(float(input("Computer: ")))
-    max_maths = int(float(input("Maths: ")))
-    max_science = int(float(input("Science: ")))
-    max_sst = int(float(input("SST: ")))
-    max_english = int(float(input("English: ")))
-    max_s = int(float(input("Sanskrit: ")))
-    max_h =  int(float(input("Hindi: ")))
-    
+    # m Input
+    print("\nEnter mimum Marks for each subject:")
+    m_comp = int(float(input("Computer: ")))
+    m_ma = int(float(input("Mathematics: ")))
+    m_sc = int(float(input("Science: ")))
+    m_ss = int(float(input("Social Studies: ")))
+    m_eng = int(float(input("English: ")))
+    m_s = int(float(input("Sanskrit: ")))
+    m_h =  int(float(input("Hindi: ")))
+    # Score Input 
     print("\nEnter Marks You Obtained:")
-    c = int(float(input("comp: ")))
-    maths = int(float(input("Maths: ")))
-    science = int(float(input("Science: ")))
-    sst = int(float(input("SST: ")))
-    english = int(float(input("English: ")))
+    c = int(float(input("Computer: ")))
+    ma = int(float(input("Mathematics: ")))
+    sc = int(float(input("Science: ")))
+    ss = int(float(input("Social Studies: ")))
+    eng = int(float(input("English: ")))
     s= int(float(input("Sanskrit: ")))
     h=  int(float(input("Hindi: ")))
-    
     # Net Calculations
-    total_max = max_comp + max_maths + max_science + max_sst + max_english + max_s + max_h
-    total_obtained = c + maths + science + sst + english + s + h
-    percentage = (total_obtained / total_max) * 100
-    
+    total_m = m_comp + m_ma + m_sc + m_ss + m_eng + m_s + m_h
+    total_obtained = c + ma + sc + ss + eng + s + h
+    percentage = (total_obtained / total_m) * 100
     # Final Output
-    print(f"Name: {name}")
-    print(f"Class: {student_class}")
-    print(f"Section: {section}")
-    print(f"Total Marks: {total_obtained}/{total_max}")
-    print(f"Percentage: {percentage:.2f}%")
-
-    print('ENDS')
+    print( name, "of" , student_class , section , "has scored totally" , total_obtained , "out of" , total_m , "and final score is", percentage , "%")
+    print("\n")
+    print("Program Ends Here")
 
 
 else:
     section = input("Enter Section: ")
-    print("\nEnter Maximum Marks for each subject:")
-    max_ai = int(float(input("AI: ")))
-    max_maths = int(float(input("Maths: ")))
-    max_science = int(float(input("Science: ")))
-    max_sst = int(float(input("SST: ")))
-    max_english = int(float(input("English: ")))
-    max_lang = int(float(input("Sanskrit/Hindi: ")))
-    
+    # m Input
+    print("\nEnter mimum Marks for each subject:")
+    m_ai = int(float(input("Artificial Intelligence or IT: ")))
+    m_ma = int(float(input("Mathematics: ")))
+    m_sc = int(float(input("Science: ")))
+    m_ss = int(float(input("Social Studies: ")))
+    m_eng = int(float(input("English: ")))
+    m_l = int(float(input("Sanskrit/Hindi: ")))
+    # Score Input
     print("\nEnter Marks Obtained:")
-    ai = int(float(input("AI: ")))
-    maths = int(float(input("Maths: ")))
-    science = int(float(input("Science: ")))
-    sst = int(float(input("SST: ")))
-    english = int(float(input("English: ")))
-    lang = int(float(input("Sanskrit/Hindi: ")))
-    
-    # Calculations
-    total_max = max_ai + max_maths + max_science + max_sst + max_english + max_lang
-    total_obtained = ai + maths + science + sst + english + lang
-    percentage = (total_obtained / total_max) * 100
-    # Output
-    
-    print(f"Name: {name}")
-    print(f"Class: {student_class}")
-    print(f"Section: {section}")
-    print(f"Total Marks: {total_obtained}/{total_max}")
-    print(f"Percentage: {percentage:.2f}%")
+    ai = int(float(input("AI or IT: ")))
+    ma = int(float(input("Mathematics: ")))
+    sc = int(float(input("Science: ")))
+    ss = int(float(input("Social Studies: ")))
+    eng = int(float(input("English: ")))
+    l = int(float(input("Sanskrit/Hindi: ")))
+    # Net Calculations
+    total_m = m_ai + m_ma + m_sc + m_ss + m_eng + m_l
+    total_obtained = ai + ma + sc + ss + eng + l
+    percentage = (total_obtained / total_m) * 100
+    # Final Output
+    print( name, "of" , student_class , section , "has scored totally" , total_obtained , "out of" , total_m , "and final score is", percentage , "%")
+    print("\n")
+    print("Program Ends Here")
